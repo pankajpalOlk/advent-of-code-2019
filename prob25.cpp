689db0c5a6a291e6141b01fb5437e1060dafe98b
pankajpalOlk <35917079+pankajpalOlk@users.noreply.github.com>
2020-01-05 11:39:55 +0530

    Update prob25.cpp

--- prob25.cpp
+++ prob25.cpp
@@ -33,7 +33,7 @@ std::vector<long> GetComputerInputs(const string& inputString)
 // user input when input queue is empty.
 // Figured out that Molten Lava, Infinite loop, Giant EM, Escape Pod and Photons shoulnt be taken
 // That left me with 8 options:
-// Sand, Hat, Prime number, mug, weather machine, astronaut ice cream, mutex, boulder.
+// Sand, Hat, Prime number, mug, weather machine, astronaut ice cream, mutext, boulder
 void Problem1(const vector<long>& vect)
 {
     Computer computer;
@@ -74,10 +74,92 @@ void Problem1(const vector<long>& vect)
     cout << endl;
 }
 
+void Problem1_AutoSolve(const vector<long>& vect)
+{
+    // First take every valid item to the penultimate node
+    // and then try all the combinations
+
+    vector<string> validItems{"astronaut ice cream", "prime number", "weather machine", "mug", "festive hat", "sand", "mutex", "boulder"};
+
+    Computer computer;
+    computer.vect = vect;
+
+    string inputStr = "east\n";
+    inputStr += "east\n";
+    inputStr += "north\n";
+    inputStr += "east\n";
+    inputStr += "east\n";
+    inputStr += "take astronaut ice cream\n";
+    inputStr += "west\n";
+    inputStr += "west\n";
+    inputStr += "south\n";
+    inputStr += "west\n";
+    inputStr += "north\n";
+    inputStr += "take weather machine\n";
+    inputStr += "north\n";
+    inputStr += "take mug\n";
+    inputStr += "south\n";
+    inputStr += "south\n";
+    inputStr += "west\n";
+    inputStr += "north\n";
+    inputStr += "take festive hat\n";
+    inputStr += "east\n";
+    inputStr += "take prime number\n";
+    inputStr += "west\n";
+    inputStr += "west\n";
+    inputStr += "take sand\n";
+    inputStr += "east\n";
+    inputStr += "south\n";
+    inputStr += "south\n";
+    inputStr += "south\n";
+    inputStr += "take mutex\n";
+    inputStr += "south\n";
+    inputStr += "take boulder\n";
+    inputStr += "east\n";
+    inputStr += "south\n";
+
+    for (auto s : validItems)
+    {
+        inputStr += "drop " + s + "\n";
+    }
+
+    for (int i = 0; i < 256; i++)
+    {
+        set<string> set;
+        for (int j = 0; j < 8; j++) {
+            if (((i >> j) & 1) == 1)
+            {
+                set.insert(validItems[j]);
+            }
+        }
+
+        for (auto s : set)
+        {
+            inputStr += "take " + s + "\n";
+        }
+
+        inputStr += "east\n";
+
+        for (auto s : set)
+        {
+            inputStr += "drop " + s + "\n";
+        }
+    }
+
+    computer.setInputArray(GetComputerInputs(inputStr));
+
+    while (!computer.done)
+    {
+        cout << (char)computer.calculate();
+    }
+
+    cout << endl;
+}
+
 int main()
 {
     vector<long> vect;
-    fstream in("/Users/pankajpal/advent/prob25_input.txt");
+    fstream in("prob25_input.txt");
 
     long number = 0;
     char ignore;
@@ -95,6 +177,8 @@ int main()
     }
 
     Problem1(vect);
+
+    Problem1_AutoSolve(vect);
 
     return 0;
 }
